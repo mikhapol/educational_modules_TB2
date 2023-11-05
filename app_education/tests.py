839ac990323885427ad4ce075be9ddd971534ddc
mikhapol_linux <mikhapol@bk.ru>
2023-11-05 18:52:13 +0300

    Написание тестов.

--- app_education/tests.py
+++ app_education/tests.py
@@ -1,3 +1,124 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from app_education.models import Education
+from users.models import User
+
+
+class EducationTestCase(APITestCase):
+    """Тестирование образовательного модуля"""
+
+    def setUp(self):
+        """Подготовка данных перед каждым тестом"""
+
+        # Создание пользователя для тестирования
+        self.user = User.objects.create(email='test@test.ru',
+                                        is_staff=True,
+                                        is_superuser=True,
+                                        is_active=True)
+
+        self.user.set_password('qwerty')  # Устанавливаем пароль
+        self.user.save()  # Сохраняем изменения пользователя в базе данных
+
+        # Создание привычки для тестирования
+        self.education = Education.objects.create(
+            user=self.user,
+            serial=1,
+            title="Простое обучение",
+            desc="Тест обучения",
+        )
+
+        # Запрос токена для авторизации
+        response = self.client.post('/users/token/', data={'email': self.user.email, 'password': 'qwerty'})
+
+        self.access_token = response.data.get('access')  # Токен для авторизации
+
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + self.access_token)  # Авторизация пользователя
+
+    def test_create_education(self):
+        """Тестирование создания образовательного модуля"""
+
+        # Данные для создания образовательного модуля
+        data = {
+            "user": 1,
+            "serial": 2,
+            "title": "Тест",
+            "desc": "Test",
+        }
+
+        response = self.client.post(reverse('app_education:education_create'), data=data)  # Отправка запроса
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)  # Проверка статуса ответа
+
+        self.assertEqual(Education.objects.all().count(), 2)  # Проверка наличия в базе данных новой записи
+
+    def test_list_education(self):
+        """Тестирование списка просмотра образовательных модулей"""
+
+        response = self.client.get(
+            reverse('app_education:educations'))  # Запрос на получение списка образовательных модулей
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)  # Проверка ответа на запрос
+
+        # Проверка корректности выводимых данных
+        self.assertEqual(response.json(), [{
+            "user": 1,
+            "serial": 1,
+            "title": "Простое обучение",
+            "desc": "Тест обучения",
+        }])
+
+    def test_update_education(self):
+        """Тестирование редактирования образовательного модуля"""
+
+        # Данные для обновления образовательного модуля
+        data = {
+            "user": 1,
+            "serial": 1,
+            "title": "Простое",
+            "desc": "Тест",
+        }
+
+        # Запрос на обновление образовательного модуля
+        response = self.client.put(reverse('app_education:education_update', args=[self.education.pk]), data=data)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)  # Проверка статуса ответа
+
+        # Проверка корректности выводимых данных
+        self.assertEqual(response.json(),
+                         {
+                             "id": 1,
+                             "user": 1,
+                             "serial": 1,
+                             "title": "Простое",
+                             "desc": "Тест",
+                         })
+
+    def test_get_education_by_id(self):
+        """Тестирование получения образовательного модуля по id"""
+
+        # Запрос на получение образовательного модуля по id
+        response = self.client.get(reverse('app_education:education', args=[self.education.pk]))
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)  # Проверка статуса ответа
+
+        # Проверка корректности выводимых данных
+        self.assertEqual(response.json(),
+                         {
+                             "id": 1,
+                             "user": 1,
+                             "serial": 1,
+                             "title": "Простое обучение",
+                             "desc": "Тест обучения",
+                         })
+
+    def test_destroy_education(self):
+        """Тестирование удаления образовательного модуля"""
+
+        # Запрос на удаление образовательного модуля
+        response = self.client.delete(reverse('app_education:education_delete', args=[self.education.pk]))
+
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)  # Проверка статуса ответа
+
+        self.assertEqual(Education.objects.all().count(), 0)  # Проверка количества записей образовательных модулей в БД
--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,114 @@
-from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APITestCase
+from users.models import User
 
-# Create your tests here.
+
+class UsersTestCase(APITestCase):
+    """Тестирование пользователей"""
+
+    def setUp(self):
+        """Подготовка данных перед каждым тестом"""
+
+        # Создание пользователя для тестирования
+        self.user = User.objects.create(email='test@test.ru',
+                                        is_staff=True,
+                                        is_superuser=True,
+                                        is_active=True)
+
+        self.user.set_password('qwerty')  # Устанавливаем пароль
+        self.user.save()  # Сохраняем изменения пользователя в базе данных
+
+        # Запрос токена для авторизации
+        response = self.client.post('/users/token/', data={'email': self.user.email, 'password': 'qwerty'})
+
+        self.access_token = response.data.get('access')  # Токен для авторизации
+
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + self.access_token)  # Авторизация пользователя
+
+    def test_create_users(self):
+        """Тестирование создания пользователя"""
+
+        # Данные для создания пользователя
+        data = {
+            "email": 'tests@test.ru',
+            "first_name": "Тест",
+            "last_name": "Тестов",
+            "password": "Test",
+        }
+
+        response = self.client.post('/users/user/', data=data)  # Отправка запроса
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)  # Проверка статуса ответа
+
+        self.assertEqual(User.objects.all().count(), 2)  # Проверка наличия в базе данных новой записи
+
+    def test_list_users(self):
+        """Тестирование списка просмотра пользователей"""
+
+        response = self.client.get('/users/user/')  # Запрос на получение списка пользователей
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)  # Проверка ответа на запрос
+
+        # Проверка корректности выводимых данных
+        self.assertEqual(response.json(), [{
+            "email": 'test@test.ru',
+            "first_name": "",
+            "last_name": "",
+            "phone": None,
+            "city": None,
+        }])
+
+    def test_update_user(self):
+        """Тестирование редактирования пользователя"""
+
+        # Данные для обновления пользователя
+        data = {
+            "email": 'test@test.ru',
+            "first_name": "first_name",
+            "last_name": "last_name",
+            "phone": "phone",
+            "city": "city",
+        }
+
+        # Запрос на обновление пользователя
+        response = self.client.put('/users/user/1/', data=data)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)  # Проверка статуса ответа
+
+        # Проверка корректности выводимых данных
+        self.assertEqual(response.json(),
+                         {
+                             "email": 'test@test.ru',
+                             "first_name": "first_name",
+                             "last_name": "last_name",
+                             "phone": "phone",
+                             "city": "city",
+                         })
+
+    def test_get_user_by_id(self):
+        """Тестирование получения пользователя по id"""
+
+        # Запрос на получение пользователя по id
+        response = self.client.get('/users/user/1/')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)  # Проверка статуса ответа
+
+        # Проверка корректности выводимых данных
+        self.assertEqual(response.json(),
+                         {
+                             "email": 'test@test.ru',
+                             "first_name": "",
+                             "last_name": "",
+                             "phone": None,
+                             "city": None,
+                         })
+
+    def test_destroy_user(self):
+        """Тестирование удаления пользователя"""
+
+        # Запрос на удаление пользователя
+        response = self.client.delete('/users/user/1/')
+
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)  # Проверка статуса ответа
+
+        self.assertEqual(User.objects.all().count(), 0)  # Проверка количества записей пользователей в БД
